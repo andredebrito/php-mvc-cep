e85ac241390d351393d81e9be3fc401d1e91ec98
André De Brito <andredebrito1990@gmail.com>
2020-06-13 12:58:31 -0300

    Footer link update

--- views/main/footer.php
+++ views/main/footer.php
@@ -1,7 +1,7 @@
 <footer>
     <div class="container-fluid text-center">
          <p class="text-center">&copy; <?= date("Y") ?> <?=CONF_SITE_NAME?>. Todos os Direitos Reservados</p>
-        <span>Desenvolvido por: <a class="text-primary font-700" href="mailto:andredebrito1990@gmail.com" target="_blank">André de Brito</a></span>
+        <span>Desenvolvido por: <a class="text-primary font-700" href="https://github.com/andredebrito" target="_blank">André de Brito</a></span>
     </div>
 </footer>
 
